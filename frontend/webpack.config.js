dfa38751396d04999a81ffcbead239aeb5e3a4ae
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-02-09 19:30:33 +0530

    adding webpack support
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,2 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            // For Js.
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    }
+                }
+            },
+            // For Css
+            {
+                test: /\.?css$/,
+                loader: ["style-loader", "css-loader"],
+                options: {
+                    url: true,
+                    import: true
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "src/index.html" })
+    ]
+}
