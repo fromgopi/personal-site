c812f9dfb10b6a04471dc1f8863b6cff6c460fce
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-02-12 18:57:44 +0530

    adding some styles
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const App = () => {
     return ( 
-        <div className="App">
-            <h1>Welcome to my site</h1>
+        <div className="App mx-auto">
+            <div className='visible lg:invisible md:p-0 pt-4 pl-4 sticky top-0 bg-zinc-900'>
+                <h1>Welcome to my site</h1>
+            </div>
+            
         </div>
     );
 }
