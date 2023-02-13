c33d8263e5ef19ceb7dad92f183afb3d64571fa3
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-02-13 12:26:49 +0530

    adding new compoents
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './component/Header/Header';
 
 const App = () => {
     return ( 
         <div className="App mx-auto">
             <div className='visible md:p-0 pt-4 pl-4 sticky top-0'>
-                <h1>Welcome to my site</h1>
+                <Header/>
             </div>
             
         </div>
---
+++ frontend/src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() { 
+    return(
+        <div className="Header">
+            <h1>Welcome to my site</h1>
+        </div>
+    );
+}
+
+export default Header;
